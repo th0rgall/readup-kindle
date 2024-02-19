48d5da0d21e3ce4b7baa0fe09a8cf0c031cd5025
Thor Galle <thorgalle@gmail.com>
2024-02-19 21:53:12 +0200

    deps: upgrade deno fresh

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,36 +2,38 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/_middleware.ts";
-import * as $3 from "./routes/api/_middleware.ts";
-import * as $4 from "./routes/api/x.ts";
-import * as $5 from "./routes/history.tsx";
-import * as $6 from "./routes/index.tsx";
-import * as $7 from "./routes/ink/add.tsx";
-import * as $8 from "./routes/login.tsx";
-import * as $9 from "./routes/logout.tsx";
-import * as $10 from "./routes/myreads.tsx";
-import * as $11 from "./routes/read/[slug].tsx";
+import * as $_404 from "./routes/_404.tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $_middleware from "./routes/_middleware.ts";
+import * as $api_middleware from "./routes/api/_middleware.ts";
+import * as $api_x from "./routes/api/x.ts";
+import * as $history from "./routes/history.tsx";
+import * as $index from "./routes/index.tsx";
+import * as $ink_add from "./routes/ink/add.tsx";
+import * as $login from "./routes/login.tsx";
+import * as $logout from "./routes/logout.tsx";
+import * as $myreads from "./routes/myreads.tsx";
+import * as $read_slug_ from "./routes/read/[slug].tsx";
+
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_404.tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/_middleware.ts": $2,
-    "./routes/api/_middleware.ts": $3,
-    "./routes/api/x.ts": $4,
-    "./routes/history.tsx": $5,
-    "./routes/index.tsx": $6,
-    "./routes/ink/add.tsx": $7,
-    "./routes/login.tsx": $8,
-    "./routes/logout.tsx": $9,
-    "./routes/myreads.tsx": $10,
-    "./routes/read/[slug].tsx": $11,
+    "./routes/_404.tsx": $_404,
+    "./routes/_app.tsx": $_app,
+    "./routes/_middleware.ts": $_middleware,
+    "./routes/api/_middleware.ts": $api_middleware,
+    "./routes/api/x.ts": $api_x,
+    "./routes/history.tsx": $history,
+    "./routes/index.tsx": $index,
+    "./routes/ink/add.tsx": $ink_add,
+    "./routes/login.tsx": $login,
+    "./routes/logout.tsx": $logout,
+    "./routes/myreads.tsx": $myreads,
+    "./routes/read/[slug].tsx": $read_slug_,
   },
   islands: {},
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
